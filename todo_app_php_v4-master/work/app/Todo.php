b964528c749315a821ac71723f500eb6f49f0190
yuki0128-1 <123346526+yuki0128-1@users.noreply.github.com>
2023-02-27 11:19:29 +0900

    Add files via upload

---
+++ todo_app_php_v4-master/work/app/Todo.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace MyApp;
+
+class Todo
+{
+  private $pdo;
+
+  public function __construct($pdo)
+  {
+    $this->pdo = $pdo;
+    Token::create();
+  }
+
+  public function processPost()
+  {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      Token::validate();
+      $action = filter_input(INPUT_GET, 'action');
+    
+      switch ($action) {
+        case 'add':
+          $id = $this->add();
+          header('Content-Type: application/json');
+          echo json_encode(['id' => $id]);
+          break;
+        case 'toggle':
+          $isDone = $this->toggle();
+          header('Content-Type: application/json');
+          echo json_encode(['is_done' => $isDone]);
+          break;
+        case 'delete':
+          $this->delete();
+          break;
+        case 'purge':
+          $this->purge();
+          break;
+        default:
+          exit;
+      }
+
+      exit;
+    }
+  }
+
+  private function add()
+  {
+    $title = trim(filter_input(INPUT_POST, 'title'));
+    if ($title === '') {
+      return;
+    }
+  
+    $stmt = $this->pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
+    $stmt->bindValue('title', $title, \PDO::PARAM_STR);
+    $stmt->execute();
+    return (int) $this->pdo->lastInsertId();
+  }
+  
+  private function toggle()
+  {
+    $id = filter_input(INPUT_POST, 'id');
+    if (empty($id)) {
+      return;
+    }
+
+    $stmt = $this->pdo->prepare("SELECT * FROM todos WHERE id = :id");
+    $stmt->bindValue('id', $id, \PDO::PARAM_INT);
+    $stmt->execute();
+    $todo = $stmt->fetch();
+    if (empty($todo)) {
+      header('HTTP', true, 404);
+      exit;
+    }
+  
+    $stmt = $this->pdo->prepare("UPDATE todos SET is_done = NOT is_done WHERE id = :id");
+    $stmt->bindValue('id', $id, \PDO::PARAM_INT);
+    $stmt->execute();
+
+    return (boolean) !$todo->is_done;
+  }
+  
+  private function delete()
+  {
+    $id = filter_input(INPUT_POST, 'id');
+    if (empty($id)) {
+      return;
+    }
+  
+    $stmt = $this->pdo->prepare("DELETE FROM todos WHERE id = :id");
+    $stmt->bindValue('id', $id, \PDO::PARAM_INT);
+    $stmt->execute();
+  }
+
+  private function purge()
+  {
+    $this->pdo->query("DELETE FROM todos WHERE is_done = 1");
+  }
+
+  public function getAll()
+  {
+    $stmt = $this->pdo->query("SELECT * FROM todos ORDER BY id DESC");
+    $todos = $stmt->fetchAll();
+    return $todos;
+  }
+}
---
+++ todo_app_php_v4-master/work/public/index.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once(__DIR__ . '/../app/config.php');
+
+use MyApp\Database;
+use MyApp\Todo;
+use MyApp\Utils;
+
+$pdo = Database::getInstance();
+
+$todo = new Todo($pdo);
+$todo->processPost();
+$todos = $todo->getAll();
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>My Todos</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <main data-token="<?= Utils::h($_SESSION['token']); ?>">
+    <header>
+      <h1>Todos</h1>
+      <span class="purge">Purge</span>
+    </header>
+
+    <form>
+      <input type="text" name="title" placeholder="Type new todo.">
+    </form>
+
+    <ul>
+      <?php foreach ($todos as $todo): ?>
+      <li data-id="<?= Utils::h($todo->id); ?>">
+        <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+        <span><?= Utils::h($todo->title); ?></span>
+        <span class="delete">x</span>
+      </li>
+      <?php endforeach; ?>
+    </ul>
+  </main>
+
+  <script src="js/main.js"></script>
+</body>
+</html>
